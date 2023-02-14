6cd9847b237ea3489e4169decc6e1710caa7c44c
shubham <shubham@SF-CPU-492.local>
2023-02-14 13:54:25 +0530

    daily work 1.0 14/2

---
+++ SplashScreenShubham/SplashScreenShubham/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SplashScreenShubham
+//
+//  Created by Shubham Bhatt on 14/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
