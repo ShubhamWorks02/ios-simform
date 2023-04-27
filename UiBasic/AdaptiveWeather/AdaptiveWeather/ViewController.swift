0a3e2aa772b6e342d34a0d10978216c81ad7954f
Shubham <shubham.work2002@gmail.com>
2023-04-27 20:47:41 +0530

    size class orientation

---
+++ UiBasic/AdaptiveWeather/AdaptiveWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdaptiveWeather
+//
+//  Created by Shubham Bhatt on 27/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
