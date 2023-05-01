3c41d917893153e387fb1dd2195c7dd4d17efc52
Shubham <shubham.work2002@gmail.com>
2023-05-01 20:03:35 +0530

    Ui component start

---
+++ UiBasic/UiComponent/UiComponent/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  UiComponent
+//
+//  Created by Shubham Bhatt on 01/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var didTap = true
+    @IBOutlet weak var hyperlinkLabel: UILabel!
+    @IBOutlet weak var labelWithLink: UILabel!
+    @IBOutlet weak var textLabelOutlet: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textLabelOutlet.text = "Runtime text change when clicked"
+        textLabelOutlet.isHighlighted = true
+        
+        textLabelOutlet.backgroundColor = UIColor.blue
+        let tap = UITapGestureRecognizer(target: self, action: #selector(changeBgColour(sender: )))
+        textLabelOutlet.isUserInteractionEnabled = true
+        textLabelOutlet.addGestureRecognizer(tap)
+        do {
+            let str = try NSAttributedString(markdown: "*github* [link](https://github.com/) ")
+            hyperlinkLabel.attributedText = str
+        }
+        catch {
+            
+        }
+        
+//        let text =
+//        let word = (text as NSString).range(of: "Word")
+//        labelWithLink.attributedText =
+        // Do any additional setup after loading the view.
+    }
+
+    @objc func changeBgColour(sender: UITapGestureRecognizer){
+        if(didTap) {
+            textLabelOutlet.backgroundColor = UIColor.yellow
+            didTap = false
+        } else {
+            textLabelOutlet.backgroundColor = UIColor.blue
+            didTap = true
+        }
+    }
+}
+
