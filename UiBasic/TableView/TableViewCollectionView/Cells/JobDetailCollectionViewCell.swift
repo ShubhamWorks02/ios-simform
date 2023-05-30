c1284f446e3ff6a67db566d1c35af24469f072f5
Shubham <shubham.work2002@gmail.com>
2023-05-30 14:15:52 +0530

    UIscreen started

---
+++ UiBasic/TableView/TableViewCollectionView/Cells/JobDetailCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  JobDetailCollectionViewCell.swift
+//  TableViewCollectionViewCollectionView
+//
+//  Created by Shubham Bhatt on 29/05/23.
+//
+
+import UIKit
+
+class JobDetailCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ UiBasic/TableView/TableViewCollectionView/ViewControllers/UIDemoScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UIDemoScreenViewController.swift
+//  TableViewCollectionViewCollectionView
+//
+//  Created by Shubham Bhatt on 30/05/23.
+//
+
+import UIKit
+
+class UIDemoScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
