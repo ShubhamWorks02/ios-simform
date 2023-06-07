0fab8bc303db7b4b62c4d0d1b08bc29a748b6d86
Shubham <shubham.work2002@gmail.com>
2023-06-07 16:16:32 +0530

    feat: TE7 - T1040: IOS-KT(Components)

--- UiBasic/ComponentsKT/ComponentsKT/Helper/AppConstants.swift
+++ UiBasic/ComponentsKT/ComponentsKT/Helper/AppConstants.swift
@@ -17,4 +17,46 @@ struct Constants {
         static let headerTableViewCell = "HeaderTableViewCell"
     }
     
+    struct VCs {
+        static let formViewController = "FormViewController"
+    }
+    
+    struct StoryBoards {
+        static let main = "Main"
+    }
+    
+    struct DocStatuses {
+        static let approved = "Approved"
+        static let awaitingApproval = "Awaiting Approval"
+    }
+    
+    struct Colors {
+        static let statusBtnApprovedColor = "statusBtnApprovedColor"
+        static let statusBtnAwaitingColor = "statusBtnAwaitingColor"
+        static let headerThemeColor = "headerThemeColor"
+    }
+    
+    struct TableViewConstants {
+        static let heightForHeader = 29.0
+    }
+    
+    struct CollectionViewConstants {
+        static let numberOfItemsInSection = 3
+    }
+    
+    struct Strings {
+        static let lblUserNameStr = "Davis\nBapista"
+        static let lblFilterStr = "Applied Filters (0)"
+        static let txtFirstNameStr = "First Name"
+        static let txtLastNameStr = "Last Name"
+        static let txtEmailStr = "Email"
+        static let txtJobTitleStr = "Job Title"
+        static let txtCompanyNameStr = "Company Name"
+        static let imgCameraStr = "camera"
+        static let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        static let emailPredicate = "SELF MATCHES %@"
+        static let alertTitle = "Invalid Input"
+        static let alertAction = "OK"
+    }
+    
 }
